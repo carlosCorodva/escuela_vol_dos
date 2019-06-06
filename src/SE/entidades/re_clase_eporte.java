1c366b917fd7f89e3752c10385fc7ef3e3b59f70
carlosCorodva <46405972+carlosCorodva@users.noreply.github.com>
2019-06-05 19:15:37 -0500

    carlos cordova 44 05-06-2019

--- src/SE/entidades/re_clase_eporte.java
+++ src/SE/entidades/re_clase_eporte.java
@@ -117,6 +117,16 @@ public class re_clase_eporte {
         this.field18 = field18;
     }
 
+    public re_clase_eporte(String field1, String field2, String field3, String field4, String field5, String field6, String field7) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+        this.field4 = field4;
+        this.field5 = field5;
+        this.field6 = field6;
+        this.field7 = field7;
+    }
+
     public String getField1() {
         return field1;
     }
--- src/SE/views/reportes/ReporteAlumnosCalificacionActualForm.java
+++ src/SE/views/reportes/ReporteAlumnosCalificacionActualForm.java
@@ -565,7 +565,7 @@ public class ReporteAlumnosCalificacionActualForm extends javax.swing.JDialog {
     public void imprimirSq() {
         ArrayList lista = new ArrayList();
         for (int i = 0; i < jtSegundoQ.getRowCount(); i++) {
-            re_clase_eporte tabla = new re_clase_eporte(txtAlumno.getText(), txtCurso.getText(), txtPeriodo.getText(), txtPromedio.getText(), jtPrimerQ.getValueAt(i, 0).toString(), jtPrimerQ.getValueAt(i, 1).toString(), jtPrimerQ.getValueAt(i, 2).toString(), jtPrimerQ.getValueAt(i, 3).toString(), jtPrimerQ.getValueAt(i, 4).toString(), jtPrimerQ.getValueAt(i, 6).toString(), jtPrimerQ.getValueAt(i, 7).toString(), jtPrimerQ.getValueAt(i, 8).toString(), jtPrimerQ.getValueAt(i, 5).toString(), jtPrimerQ.getValueAt(i, 9).toString());
+            re_clase_eporte tabla = new re_clase_eporte(txtAlumno.getText(), txtCurso.getText(), txtPeriodo.getText(), txtPromedio.getText(), jtSegundoQ.getValueAt(i, 0).toString(), jtSegundoQ.getValueAt(i, 1).toString(), jtSegundoQ.getValueAt(i, 2).toString(), jtSegundoQ.getValueAt(i, 3).toString(), jtSegundoQ.getValueAt(i, 4).toString(), jtSegundoQ.getValueAt(i, 6).toString(), jtSegundoQ.getValueAt(i, 7).toString(), jtSegundoQ.getValueAt(i, 8).toString(), jtSegundoQ.getValueAt(i, 5).toString(), jtSegundoQ.getValueAt(i, 9).toString());
             lista.add(tabla);
         }
         try {
@@ -587,7 +587,7 @@ public class ReporteAlumnosCalificacionActualForm extends javax.swing.JDialog {
     public void promedio() {
         ArrayList lista = new ArrayList();
         for (int i = 0; i < jtPromedio.getRowCount(); i++) {
-            re_clase_eporte tabla = new re_clase_eporte(txtAlumno.getText(), txtCurso.getText(), txtPeriodo.getText(), txtPromedio.getText(), jtPrimerQ.getValueAt(i, 0).toString(), jtPrimerQ.getValueAt(i, 1).toString(), jtPrimerQ.getValueAt(i, 2).toString(), jtPrimerQ.getValueAt(i, 3).toString(), jtPrimerQ.getValueAt(i, 4).toString(), jtPrimerQ.getValueAt(i, 5).toString(), jtPrimerQ.getValueAt(i, 6).toString());
+            re_clase_eporte tabla = new re_clase_eporte(txtAlumno.getText(), txtCurso.getText(), txtPeriodo.getText(), txtPromedio.getText(), jtPromedio.getValueAt(i, 0).toString(), jtPromedio.getValueAt(i, 1).toString(), jtPromedio.getValueAt(i, 2).toString(), jtPromedio.getValueAt(i, 3).toString(), jtPromedio.getValueAt(i, 4).toString(), jtPromedio.getValueAt(i, 5).toString(), jtPromedio.getValueAt(i, 6).toString());
             lista.add(tabla);
         }
         try {
