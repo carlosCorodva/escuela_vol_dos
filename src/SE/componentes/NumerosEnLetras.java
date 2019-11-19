ca55ac223ee0ad2e6af251c9a30024adf3764d3c
carlosCorodva <46405972+carlosCorodva@users.noreply.github.com>
2019-11-19 13:05:05 -0500

    carlos cordova

---
+++ src/SE/componentes/NumerosEnLetras.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SE.componentes;
+
+/**
+ *
+ * @author carlos
+ */
+public class NumerosEnLetras {
+
+    public String letras(double numero) {
+        String valor = "";
+        if(numero == 0.00){
+            valor = "CERO COMA CERO";
+        }
+        if(numero == 0.01){
+            valor = "CERO COMA UNO";
+        }
+        if(numero == 0.02){
+            valor = "CERO COMA DOS";
+        }
+        if(numero == 0.03){
+            valor = "CERO COMA TRES";
+        }
+        if(numero == 0.04){
+            valor = "CERO COMA CUATRO";
+        }
+        if(numero == 0.05){
+            valor = "CERO COMA CINCO";
+        }
+        if(numero == 0.06){
+            valor = "CERO COMA SEIS";
+        }
+        if(numero == 0.07){
+            valor = "CERO COMA SIETE";
+        }
+        if(numero == 0.08){
+            valor = "CERO COMA OCHO";
+        }
+        if(numero == 0.09){
+            valor = "CERO COMA NUEVE";
+        }
+        if(numero == 0.10){
+            valor = "CERO COMA DIEZ";
+        }
+        
+        if(numero == 0.11){
+            valor = "CERO COMA ONCE";
+        }
+        if(numero == 0.12){
+            valor = "CERO COMA DOCE";
+        }
+        if(numero == 0.13){
+            valor = "CERO COMA TRECE";
+        }
+        if(numero == 0.14){
+            valor = "CERO COMA CATORCE";
+        }
+        if(numero == 0.15){
+            valor = "CERO COMA QUINCE";
+        }
+        if(numero == 0.16){
+            valor = "CERO COMA DIECISEIS";
+        }
+        if(numero == 0.17){
+            valor = "CERO COMA DIECISIETE";
+        }
+        if(numero == 0.18){
+            valor = "CERO COMA DIECIOCHO";
+        }
+        if(numero == 0.19){
+            valor = "CERO COMA DIECINUEVE";
+        }
+        if(numero == 0.20){
+            valor = "CERO COMA VEINTE";
+        }
+        
+        if(numero == 0.21){
+            valor = "CERO COMA VEINTIUNO";
+        }
+        if(numero == 0.22){
+            valor = "CERO COMA VEINTIDOS";
+        }
+        if(numero == 0.23){
+            valor = "CERO COMA VEINTITRES";
+        }
+        if(numero == 0.24){
+            valor = "CERO COMA VEINTICUATRO";
+        }
+        if(numero == 0.25){
+            valor = "CERO COMA VEINTICINCO";
+        }
+        if(numero == 0.26){
+            valor = "CERO COMA VEINTISEIS";
+        }
+        if(numero == 0.27){
+            valor = "CERO COMA VEINTISIETE";
+        }
+        if(numero == 0.28){
+            valor = "CERO COMA VEINTIOCHO";
+        }
+        if(numero == 0.29){
+            valor = "CERO COMA VEINTINUEVE";
+        }
+        if(numero == 0.30){
+            valor = "CERO COMA TREINTA";
+        }
+        
+        return valor;
+    }
+}
